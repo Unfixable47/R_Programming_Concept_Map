6fa57dc4f04dab594030d1ff3ef6f75273e5fb07
Unfixable47 <Unfixable47@proton.me>
2023-11-26 14:17:45 -0500

    Flesh out comparative Analysis for CBCR, TUDR and WorkPlace Rights#4

--- Analysis/Trade Union Global Analysis./Comparative Analysis (Trade Union DB).R
+++ Analysis/Trade Union Global Analysis./Comparative Analysis (Trade Union DB).R
@@ -24,6 +24,7 @@ library(ggplot2)
 library(dplyr)
 library(forcats)
 
+### Collective Bargaining Coverage
 # Looping through each year from 2010 to 2020
 for (year_focus in 2015:2020) {
   # Filter the data for the selected year
@@ -46,3 +47,57 @@ for (year_focus in 2015:2020) {
   # Print the plot
   print(plot)
 }
+
+
+
+### Labor By Country Analysis.
+# Looping through each year from 2010 to 2020
+for (year_focus in 2015:2020) {
+  # Filter the data for the selected year
+  WorkplaceRight_year <- joined_full_data_set %>%
+    filter(time == year_focus)
+  
+  # Reordering the ref_area based on the Collective Bargaining Coverage
+  WorkplaceRight_year <- WorkplaceRight_year %>%
+    mutate(ref_area = fct_reorder(ref_area, `FACB`))
+  
+  # Comparative plot
+  plot <- ggplot(WorkplaceRight_year, aes(x = ref_area, y = `FACB`)) +
+    geom_bar(stat = "identity") +
+    coord_flip() +
+    labs(title = paste("Comparative Analysis of Countries Complaince with international labor law", year_focus),
+         x = "Country",
+         y = "FACB") +
+    theme_minimal()
+  
+  # Print the plot
+  print(plot)
+}
+
+
+
+for (year_focus in 2015:2020) {
+  # Filter the data for the selected year
+  UnionDensity_year <- joined_full_data_set %>%
+    filter(time == year_focus)
+  
+  # Reordering the ref_area based on the Collective Bargaining Coverage
+  UnionDensity_year <- UnionDensity_year %>%
+    mutate(ref_area = fct_reorder(ref_area, `Union Density`))
+  
+  # Comparative plot
+  plot <- ggplot(UnionDensity_year, aes(x = ref_area, y = `Union Density`)) +
+    geom_bar(stat = "identity") +
+    coord_flip() +
+    labs(title = paste("Comparative Analysis of Union Density in different countries", year_focus),
+         x = "Country",
+         y = "FACB") +
+    theme_minimal()
+  
+  # Print the plot
+  print(plot)
+  
+}
+
+
+
