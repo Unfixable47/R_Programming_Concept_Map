d76e79afc4a5b648cb67ec9c8c5be34a5394c6f5
Unfixable47 <Unfixable47@proton.me>
2023-11-27 10:47:46 -0500

    Make a file for summary statics, make sure data is clear.#6

---
+++ Analysis/Trade Union Global Analysis./summary data.R
@@ -0,0 +1,15 @@
+# joined_full_data_set
+
+library(dplyr)
+
+# Assuming your data frame is named joined_full_data_set
+result <- joined_full_data_set %>%
+  group_by(ref_area) %>%
+  summarise(
+    mean_time = mean(time, na.rm = TRUE),
+    mean_FACB = mean(FACB, na.rm = TRUE),
+    mean_Collective_Bargaining_Coverage = mean(`Collective Bargaining Coverage`, na.rm = TRUE),
+    mean_Union_Density = mean(`Union Density`, na.rm = TRUE)
+  )
+
+print(result)
