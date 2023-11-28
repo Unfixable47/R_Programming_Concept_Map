f59e52f75365ecc84eaabf1b4b9ed9aeb2e775e2
Unfixable47 <Unfixable47@proton.me>
2023-11-28 09:44:03 -0500

    created more comprehensive for loops for the time series analysis

--- Analysis/Trade Union Global Analysis./summary data.R
+++ Analysis/Trade Union Global Analysis./summary data.R
@@ -6,7 +6,6 @@ library(dplyr)
 ILO_Summary_data <- joined_full_data_set %>%
   group_by(ref_area) %>%
   summarise(
-    mean_time = mean(time, na.rm = TRUE),
     mean_National_Compliance_wth_Labour_Rights = mean(National_Compliance_wth_Labour_Rights, na.rm = TRUE),
     mean_Collective_Bargaining_Coverage = mean(`Collective Bargaining Coverage`, na.rm = TRUE),
     mean_Union_Density = mean(`Union Density`, na.rm = TRUE)
